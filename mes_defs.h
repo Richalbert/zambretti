39aa1b5bad4e48e267644596aa87c605c88765a3
Richalbert <richalbert@free.fr>
2019-04-24 11:23:29 +0200

    algo fonctionne

---
+++ mes_defs.h
@@ -0,0 +1,124 @@
+/*
+ * mes_defs.h
+ */
+
+
+struct Forecast {
+        char letter;
+        std::string description;
+};
+
+
+struct Barometer {
+        int number;
+        char letter;
+        std::string text;
+        int pressure;
+};
+
+
+#define RISING_SIZE 14
+#define STEADY_SIZE 11
+#define FALLING_SIZE 10
+
+#define RISING_MIN 947
+#define RISING_MAX 1030
+
+#define STEADY_MIN 960
+#define STEADY_MAX 1033
+
+#define FALLING_MIN 995 
+#define FALLING_MAX 1050
+
+enum Season { WINTER, SUMMER };
+enum Tendance { A_LA_BAISSE, STABLE, A_LA_HAUSSE };
+
+
+/* prototypes de fonctions */
+//char correction(enum Season, char letter);
+int p_to_p0 (int);
+char p_to_letter_in_rising(int);
+char p_to_letter_in_steady(int);
+char p_to_letter_in_falling(int);
+char correction_ete(char);
+char correction_hiver(char);
+
+
+
+struct Forecast forecast[26]={
+                {'A',"Settled Fine"},
+                {'B',"Fine Weather"},
+                {'C',"Becoming Fine"},
+                {'D',"Fine Becoming Less Settled"},
+                {'E',"Fine, Possibly showers"},
+                {'F',"Fairly Fine, Improving"},
+                {'G',"Fairly Fine, Possibly showers, early"},
+                {'H',"Fairly Fine Showery Later"},
+                {'I',"Showery Early, Improving"},
+                {'J',"Changeable Mending"},
+                {'K',"Fairly Fine , Showers likely"},
+                {'L',"Rather Unsettled Clearing Later"},
+                {'M',"Unsettled, Probably Improving"},
+                {'N',"Showery Bright Intervals"},
+                {'O',"Showery Becoming more unsettled"},
+                {'P',"Changeable some rain"},
+                {'Q',"Unsettled, short fine Intervals"},
+                {'R',"Unsettled, Rain later"},
+                {'S',"Unsettled, rain at times"},
+                {'T',"Very Unsettled, Finer at times"},
+                {'U',"Rain at times, worse later."},
+                {'V',"Rain at times, becoming very unsettled"},
+                {'W',"Rain at Frequent Intervals"},
+                {'X',"Very Unsettled, Rain"},
+                {'Y',"Stormy, possibly improving"},
+                {'Z',"Stormy, much rain"}
+        };
+
+
+struct Barometer falling[10]={
+                {0,'\0',"",0},
+                {1, 'A',"Settled Fine", 1050},
+                {2, 'B',"Fine Weather", 1040},
+                {3, 'D',"Fine Becoming Less Settled",1024},
+                {4, 'H',"Fairly Fine Showery Later", 1018},
+                {5, 'O',"Showery Becoming more unsettled",1010},
+                {6, 'R',"Unsettled, Rain later",1004},
+                {7, 'U',"Rain at times, worse later." , 998},
+                {8, 'V',"Rain at times, becoming very unsettled",991},
+                {9, 'X',"Very Unsettled, Rain", 985}
+        };
+
+        struct Barometer steady[11]={
+                {0,'\0',"",0},
+                {1, 'A',"Settled Fine",         1033},
+                {2, 'B',"Fine Weather",         1023},
+                {3, 'E',"Fine, Possibly showers", 1014},
+                {4, 'K',"Fairly Fine , Showers likely",1008},
+                {5, 'N',"Showery Bright Intervals", 1000},
+                {6, 'P',"Changeable some rain", 994},
+                {7, 'S',"Unsettled, rain at times", 989},
+                {8, 'W',"Rain at Frequent Intervals",981},
+                {9, 'X',"Very Unsettled, Rain", 974},
+                {10,'Z',"Stormy, much rain", 960}
+
+        };
+
+
+       struct Barometer rising[14]={
+                {0,'\0',"",0},
+                {1, 'A',"Settled Fine", 1030},
+                {2, 'B',"Fine Weather", 1022},
+                {3, 'C',"Becoming Fine",1012},
+                {4, 'F',"Fairly Fine, Improving",1007},
+                {5, 'G',"Fairly Fine, Possibly showers, early", 1000},
+                {6, 'I',"Showery Early, Improving", 995},
+                {7, 'J',"Changeable Mending", 990},
+                {8, 'L',"Rather Unsettled Clearing Later",984},
+                {9, 'M',"Unsettled, Probably Improving", 978},
+                {10, 'Q',"Unsettled, short fine Intervals",970},
+                {11,'T',"Very Unsettled, Finer at times", 965},
+                {12,'Y',"Stormy, possibly improving", 959},
+                {13,'Z',"Stormy, much rain",947}
+
+        };
+
---
+++ struct.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <string>
+#include "mes_defs.h"
+
+
+
+
+int main(int argc, char* argv[]) {
+
+
+
+
+
+	std::cout << "the forecast letter scale" << std::endl;
+	for (int i=0; i<26; i++){
+		std::cout << forecast[i].letter << "  " << forecast[i].description << std::endl;
+	}
+
+
+	std::cout << "\nbarometer falling" << std::endl;
+	for (int i=1; i<FALLING_SIZE ; i++){
+		std::cout << falling[i].letter << " " << falling[i].text << " " << falling[i].pressure << std::endl;
+	}
+
+	std::cout << "\nbarometer steady" << std::endl;
+	for (int i=1; i<STEADY_SIZE ; i++){
+		std::cout << steady[i].letter << " " << steady[i].text << " " << steady[i].pressure << std::endl;
+	}
+
+	std::cout << "\nbarometer rising" << std::endl;
+	for (int i=1; i<RISING_SIZE ; i++){
+		std::cout << rising[i].number << " " << rising[i].letter << " " << rising[i].text << " " << rising[i].pressure << std::endl;
+	}
+
+	/********************* TODO *****************************************/
+
+	/*
+	 * A Winter falling generally results in a Z value lower by 1 unit compared with a Summer falling pressure. 
+	 * Similarly a Summer rising, improves the prospects by 1 unit over a Winter rising.
+	 *
+	 */ 
+
+	int p=1015;
+	int deltaP;
+	Season season = SUMMER;
+	Tendance tendance=A_LA_HAUSSE;
+	char letter='\0';
+
+	switch(tendance) {
+
+		case A_LA_HAUSSE:	
+			letter=p_to_letter_in_rising(p);
+			std::cout << "p_to_letter_in_rising(" << p << ") -> " << letter << std::endl;
+			if (season == SUMMER) {
+				// letter++ dans le tableau rising
+				letter=correction_ete(letter);
+				std::cout << "correction_ete --> " << letter << std::endl; 
+			}			
+			break;
+
+		case STABLE:
+			letter=p_to_letter_in_steady(p);
+			break;
+
+		case A_LA_BAISSE:
+			letter=p_to_letter_in_falling(p);
+			if (season == WINTER) {
+				// letter-- dans le tableau falling
+				letter=correction_hiver(letter);
+			}	
+			break;
+	}
+
+	std::cout << "pression = " << p << "   -> lettre : " << letter << std::endl;
+
+
+
+
+
+	return 0;
+}
+
+//entree: la lettre
+//sortie: la lettre+1 dans le tableau rising
+//-------------------------------------------
+char correction_ete(char letter) {
+	char z='\0';
+
+	// on trouve la lettre dans le tableau
+	int j=0;
+	for (int i=1; i<RISING_SIZE; i++) {
+		j=i;
+		z = rising[i].letter;
+		if ( letter == rising[i].letter ) { break; }
+	}
+	// on applique la correction
+	z = rising[j+1].letter;
+
+	return z;
+}
+
+
+//entree: la lettre
+//sortie: la lettre-1 dans le tableau falling
+//-------------------------------------------
+char correction_hiver(char letter) {
+	char z='\0';
+
+	// on trouve la lettre dans le tableau
+	int j=0;
+	for (int i=1; i<RISING_SIZE; i++) {
+		j=i;
+		z = falling[i].letter;
+		if ( letter == falling[i].letter ) { break; }
+	}
+	// on applique la correction
+	z = falling[j-1].letter;
+
+	return z;
+}
+
+
+//entree: la pression 
+//sortie: la lettre correspondant dans le tableau rissing
+//-------------------------------------------------------
+char p_to_letter_in_rising(int p) {
+	char z = '\0';
+
+	//TODO : check p entre MIN et MAX
+
+	for (int i=1; i<RISING_SIZE ; i++){
+		z = rising[i].letter;
+		if ( p >= rising[i].pressure ) { break; }
+	}
+	return z;
+}
+
+//entree: la pression 
+//sortie: la lettre correspondant dans le tableau steady
+//-------------------------------------------------------
+char p_to_letter_in_steady(int p) {
+	char z = '\0';
+
+	//TODO : check p entre MIN et MAX
+
+	for (int i=1; i<STEADY_SIZE ; i++){
+		z = steady[i].letter;
+		if ( p >= steady[i].pressure ) { break; }
+	}
+	return z;
+}
+
+//entree: la pression 
+//sortie: la lettre correspondant dans le tableau falling
+//-------------------------------------------------------
+char p_to_letter_in_falling(int p) {
+	char z = '\0';
+
+	//TODO : check p entre MIN et MAX
+
+	for (int i=1; i<FALLING_SIZE ; i++){
+		z = falling[i].letter;
+		if ( p >= falling[i].pressure ) { break; }
+	}
+	return z;
+}
+
+
+
+
+
+
+
+//entree : la pression du capteur
+//sortie : la pression au niveau de la mer
+//----------------------------------------
+int p_to_p0 (int p){
+	// TODO
+	return p;
+}
+
+
+/*
+
+
+
+
+	// entree : la pression
+	// sortie : la lettre ou pointeur sur la srtucture contenant la lettre
+	//
+	char pressure_to_lettre(int pression)
+
+
+	if ( deltaP < 0 ) { tendance == A_LA_BAISSE; }
+	if ( deltaP > 0 ) { tendance == A_LA_HAUSSE; }
+	if ( deltaP == 0) { tendance == STABLE; }
+
+
+
+
+	if ( season == WINTER && tendance == A_LA_BAISSE ) {
+
+		//Winter + tendance a la baisse => barometre falling
+		letter=correction_winter(&falling,letter);
+	}
+
+	if ( season == SUMMER && tendance == A_LA_HAUSSE ) {
+
+		//letter++ dans le tableau rising
+		//Summer + tendance a la hausse => barometre rising
+		letter=correction_summer(&rising,letter);
+	}
+
+
+	*/
+
+/*
+
+//letter-- dans le tableau falling
+char correction_winter(struct Barometer *barometer, ) {
+
+
+
+	}
+
+
+
+}
+
+
+
+
+*/
